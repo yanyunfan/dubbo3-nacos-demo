75f73a97399ac5a8739e091012e39ce3e3a58b0d
yanyunfan <961241246@qq.com>
2023-03-25 15:46:28 +0800

    配置

--- dubbo-provider/src/main/java/com/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/dubbo/provider/DubboProviderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo(scanBasePackages = "com.dubbo.provider.service")
+@EnableDubbo
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
